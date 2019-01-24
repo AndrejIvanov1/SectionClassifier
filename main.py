a3d3b1822a3fadcfb5a1cd37c57ecdac2be0e862
Andrej Ivanov <ivandrej@gmail.com>
2019-01-24 16:52:46 +0000

    Add a few text preprocessing steps

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from load_data import load_dataset, get_section_names
-from preprocessing import split_words
+from preprocessing import split_words, remove_stopwords, lowercase
 if __name__ == "__main__":
 	data, labels = load_dataset(get_section_names(), max_samples=4)
 	data = split_words(data)
+	data = lowercase(data)
+	data = remove_stopwords(data)
 	print(data[0])
+	print(data[10])
--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,25 @@
 from nltk.tokenize import word_tokenize
-
+from nltk.corpus import stopwords
 """
 	Parameters: data - list of paragraphs as strings
 	Returns: list of paragraphs as lists of words
 """
 def split_words(data):
 	return list(map(lambda paragraph: word_tokenize(paragraph), data))
+
+
+def lowercase(data):
+	return list(map(lambda paragraph: list(map(lambda word: word.lower(), paragraph)), data))
+
+
+def remove_stopwords(data):
+	stop_words = set(stopwords.words("english"))
+	return list(map(lambda paragraph: list(filter(lambda word: word not in stop_words, paragraph)), data))
+
+
+""" 
+def remove_punctuation(data):
+	return list(map(lambda paragraph: list(filter())
+"""
+
 
