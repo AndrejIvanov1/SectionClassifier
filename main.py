e59e038e5edf803b5ebd6d3316bae9e9daf1f642
Andrej Ivanov <ivandrej@gmail.com>
2019-01-24 17:31:21 +0000

    Add word stemming to preprocessing

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from load_data import load_dataset, get_section_names
-from preprocessing import split_words, remove_stopwords, lowercase
+from preprocessing import preprocess
 if __name__ == "__main__":
 	data, labels = load_dataset(get_section_names(), max_samples=4)
-	data = split_words(data)
-	data = lowercase(data)
-	data = remove_stopwords(data)
+
+	data = preprocess(data, split_words=True, lowercase=True, remove_stopwords=True, stem=True)
+	
 	print(data[0])
 	print(data[10])
--- preprocessing.py
+++ preprocessing.py
@@ -1,22 +1,40 @@
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
 """
 	Parameters: data - list of paragraphs as strings
 	Returns: list of paragraphs as lists of words
 """
-def split_words(data):
+def _split_words(data):
 	return list(map(lambda paragraph: word_tokenize(paragraph), data))
 
 
-def lowercase(data):
+def _lowercase(data):
 	return list(map(lambda paragraph: list(map(lambda word: word.lower(), paragraph)), data))
 
 
-def remove_stopwords(data):
+def _remove_stopwords(data):
 	stop_words = set(stopwords.words("english"))
 	return list(map(lambda paragraph: list(filter(lambda word: word not in stop_words, paragraph)), data))
 
 
+def _stem(data):
+	stemmer = PorterStemmer()
+	return list(map(lambda paragraph: list(map(lambda word: stemmer.stem(word), paragraph)), data))
+
+
+def preprocess(data, split_words=False, lowercase=False, remove_stopwords=False, stem=False):
+	if split_words:
+		data = _split_words(data)
+	if lowercase:
+		data = _lowercase(data)
+	if remove_stopwords:
+		data = _remove_stopwords(data)
+	if stem:
+		data = _stem(data)
+
+	return data
 """ 
 def remove_punctuation(data):
 	return list(map(lambda paragraph: list(filter())
