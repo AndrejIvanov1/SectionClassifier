b58bec4cccd9428376c769811bba06c7fdd0852a
Andrej Ivanov <ivandrej@gmail.com>
2019-03-12 11:58:40 +0000

    Implement xml parsing of orcid

--- create_dataset/author_dataset/orcid_parser.py
+++ create_dataset/author_dataset/orcid_parser.py
@@ -17,10 +17,37 @@ class OrcidParser:
         </contrib>
     </contrib-group>
     """
+    def parse_orcid(self, contributor):
+        orcid_id = contributor.xpath('./contrib-id[@contrib-id-type="orcid"]')
+        if orcid_id == []:
+            return None
+
+        return orcid_id[0].text
+
+    def parse_first_name(self, contributor):
+        first_name = contributor.xpath('./name/given-names')[0]
+
+        return first_name.text
+
+    def parse_last_name(self, contributor):
+        last_name = contributor.xpath('./name/surname')[0]
+
+        return last_name.text
+
     def parse_orcid_id(self):
-        contributors = self.ET.xpath("//contrib-group")
-        print(contributors)
-        sleep(1)
+        contributors = self.ET.xpath("//contrib-group")[0]
+        labeled_authors = []
+        for contributor in contributors:
+            orcid = self.parse_orcid(contributor)
+            if orcid is None:
+                continue
+
+            first_name = self.parse_first_name(contributor)
+            last_name = self.parse_last_name(contributor)
+
+            labeled_authors.append((first_name, last_name, orcid))
+
+        return labeled_authors
     """
     # <front>
     # .....
@@ -71,7 +98,14 @@ class OrcidParser:
 
 
 if __name__ == "__main__":
-    xml_content = open("example.xml", 'r').read().replace('&', '')
+    xml_content = ' <contrib-group> \
+        <contrib contrib-type="author" corresp="yes"> \
+            <name> \
+                <surname>Raufman</surname> \
+                <given-names>Jean-Pierre</given-names> \
+            </name> \
+            <contrib-id contrib-id-type="orcid">http://orcid.org/0000-0002-6340-4382</contrib-id> \
+        </contrib> \
+    </contrib-group>'
     parser = OrcidParser(xml_content)
-    parser.parse_orcid_id()
-    pass
+    print(parser.parse_orcid_id())
